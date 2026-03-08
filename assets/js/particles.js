767d51444e7cfcba2434ca4f8e932d3f1801422d
jdiego1927 <jdiego1906@gmail.com>
2026-03-07 21:55:10 -0500

    Initialize particles.js with configuration settings

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -1,1 +1,20 @@
-
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 80 },
+    color: { value: "#ffffff" },
+    shape: { type: "circle" },
+    opacity: { value: 0.5 },
+    size: { value: 3 },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 3
+    }
+  }
+});
